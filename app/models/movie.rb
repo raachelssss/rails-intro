fdfc1c94aafeeeea8ad606e54a2ba889d6022078
Rachel Seong <minkyungseong@berkeley.edu>
2022-10-07 10:01:17 +0000

    part1?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,7 +7,7 @@ class Movie < ActiveRecord::Base
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.is_A? Array 
+    if not ratings_list.nil?
       return self.where(rating: ratings_list)
     else
       return self.all
